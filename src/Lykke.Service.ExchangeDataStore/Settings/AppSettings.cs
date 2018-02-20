902f94880ef6bcb64576ee765b2d524a372512f2
Filip Lyubenov <filip.lyubenov@primeholding.com>
2018-02-20 09:06:10 +0200

    Project template commit

---
+++ src/Lykke.Service.ExchangeDataStore/Settings/AppSettings.cs
@@ -0,0 +1,11 @@
+﻿using Lykke.Service.ExchangeDataStore.Settings.ServiceSettings;
+using Lykke.Service.ExchangeDataStore.Settings.SlackNotifications;
+
+namespace Lykke.Service.ExchangeDataStore.Settings
+{
+    public class AppSettings
+    {
+        public ExchangeDataStoreSettings ExchangeDataStoreService { get; set; }
+        public SlackNotificationsSettings SlackNotifications { get; set; }
+    }
+}
---
+++ src/Lykke.Service.ExchangeDataStore/Settings/ServiceSettings/ExchangeDataStoreSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Lykke.Service.ExchangeDataStore.Settings.ServiceSettings
+{
+    public class ExchangeDataStoreSettings
+    {
+        public DbSettings Db { get; set; }
+    }
+}
